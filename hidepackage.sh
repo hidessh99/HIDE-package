be47ea71c984e137882e4630fd61d502767089bd
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-16 11:00:48 +0700

    Create hidepackage.sh

---
+++ hidepackage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#date january 2022
+# created bye hidessh.com
+
+#remove log 
+wget -q -O /usr/bin/removelog "https://raw.githubusercontent.com/hidessh99/HIDE-package/main/log.sh" && chmod +x /usr/bin/removelog
+
+
+#cronjob
+echo "0 6 * * * root removelog " >> /etc/crontab
